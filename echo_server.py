2ec3e4462faf9223933c09535b7963eab2f63324
pennyfea <pennyfea@ualberta.ca>
2019-01-11 14:01:22 -0500

    Completed echo server

--- echo_server.py
+++ echo_server.py
@@ -4,48 +4,53 @@ import os
 import sys
 from multiprocessing import Process
 
-def handle_connection(accepted_socket, address):
+class Server:
 
-        process_id = os.getpid()
-        print("Process id:", process_id)
+        def __init__(self, HOST, PORT):
+                 # Port for socket and Host
+                self.hostname = HOST
+                self.port = PORT
+        
+        def server(self):
+                # Create socket
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+                # bind the socket to host and port
+                self.sock.bind((self.hostname, self.port))
+                # become a server socket
+                self.sock.listen(5)
+
+                while True:
+                        # Accept the connection
+                        clientsocket, address = self.sock.accept()
+
+                        #Spawn multiple processes
+                        process = Process(target = handle_connection, args = (clientsocket, address))
+                        process.daemon = True
+                        process.start()
+
+def handle_connection(conn, address):
 
         print("Got connection from", address)
-        # Recieve message from the client
 
-        message = accepted_socket.recv(2024)
-        print("Server received: " + message.decode('utf-8'))
-        reply = ("Server output: " + message.decode('utf-8'))
-        if not message:
-                print("Client has been disconnected.....")
-                # Close the connection with the client
-                accepted_socket.close()
-                sys.exit()
-        # Display messags.
-        accepted_socket.sendall(str.encode(reply))
-
- 
-        
-
-def server():
-
-        # Port for socket and Host
-        HOST = 'localhost'
-        PORT = 8001
-        
-        # Create socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # bind the socket to host and port
-        sock.bind((HOST, PORT))
-        # become a server socket
-        sock.listen(5)
-        
         while True:
-                 # blocks here.
-                clientsocket, address = sock.accept()
-                # unblocked 
-                client_process = Process(target=handle_connection, args=(clientsocket, address))
-                client_process.start()
+
+                process_id = os.getpid()
+                
+                # Recieve message from the client
+                message = conn.recv(2024)
+                print("Process id:", process_id)
+                print("Server received: " + message.decode('utf-8'))
+                reply = ("Server output: " + message.decode('utf-8'))
+                if not message:
+                        print("Client has been disconnected.....")
+                        break
+                # Display messags
+                conn.sendall(str.encode(reply))
+        conn.close()
 
 if __name__ == '__main__':
-    server()
+        ser = Server('localhost', 8003)
+        print("Starting server......")
+        ser.server()
+     
