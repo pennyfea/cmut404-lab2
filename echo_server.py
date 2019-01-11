e1cabbd1a5edf58c3214b72aa4bcfb82a0e2155f
pennyfea <pennyfea@ualberta.ca>
2019-01-11 00:34:58 -0500

    Multiprocessing echo server not completed

--- echo_server.py
+++ echo_server.py
@@ -1,33 +1,56 @@
 #!/usr/bin/env python
 import socket
-import sys
+import os
+from multiprocessing import Process, current_process
 
-# Create socket
-sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def create_socket():
 
-# Port for socket and Host
-PORT = 8002
-HOST = 'localhost'
+    # Get the process id.
+    process_id = os.getpid()
+    print("Process id:", process_id)
 
-# bind the socket to host and port
-sockfd.bind((HOST, PORT))
-# become a server socket
-sockfd.listen(5)
+    # Create socket
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Establish and accept connections woth client
-(clientsocket, address) = sockfd.accept()
-while True:
+    # Port for socket and Host
+    PORT = 8005
+    HOST = 'localhost'
 
-    print("Got connection from", address)
-    # Recieve message from the client
-    message = clientsocket.recv(2024)
-    print("Server received: " + message.decode('utf-8'))
-    reply = ("Server output: " + message.decode('utf-8'))
-    if not message:
-        print("Client has been disconnected.....")
-        break
-    # Display messags.
-    clientsocket.sendall(str.encode(reply))
+    # bind the socket to host and port
+    sockfd.bind((HOST, PORT))
+    # become a server socket
+    sockfd.listen(5)
 
-# Close the connection with the client
-clientsocket.close()
+    # Establish and accept connections woth client
+    (clientsocket, address) = sockfd.accept()
+
+    start_socket(clientsocket,address)
+
+def start_socket(clientsocket,address):
+   
+    while True:
+
+
+        print("Got connection from", address)
+        # Recieve message from the client
+        message = clientsocket.recv(2024)
+        print("Server received: " + message.decode('utf-8'))
+        reply = ("Server output: " + message.decode('utf-8'))
+        if not message:
+            print("Client has been disconnected.....")
+            break
+        # Display messags.
+        clientsocket.sendall(str.encode(reply))
+
+    # Close the connection with the client
+    clientsocket.close()
+
+if __name__ == '__main__':
+
+    processes = []
+    process = Process(target = create_socket)
+    processes.append(process)
+
+    # Processes are spawned by creating a Process object and
+    # then calling its start method
+    process.start()
