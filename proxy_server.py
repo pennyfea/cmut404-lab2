cb38a336a6b2c470a7ff7c7c184475f9cf55cfd1
pennyfea <pennyfea@ualberta.ca>
2019-01-12 02:13:38 -0500

    Proxy server completed

--- proxy_server.py
+++ proxy_server.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import socket
+import os
+import sys
+from multiprocessing import Process
+
 class Proxy_Server:
 
     def __init__(self, HOST, PORT):
@@ -7,17 +11,52 @@ class Proxy_Server:
         self.host = HOST
         self.port = PORT
 
+    
+    def server(self):
         # Create socket
-        self.sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # bind the socket to a host & port
-        self.sockfd.bind((self.host,self.port))
+        # bind the socket to host and port
+        self.sock.bind((self.host, self.port))
+        # Start listening
+        self.sock.listen(5)
 
-        # become a server socket
-        self.sockfd.listen(5)
+        while True:
+            # Accept the connection
+            clientsocket, address = self.sock.accept()
 
-        # Establish and accept connections with client
-        (clientsocket, self.address) = self.sockfd.accept()
+            #Spawn multiple processes
+            process = Process(target = self.handle_connection, args = (clientsocket, address))
+            process.daemon = True
+            process.start()
 
-socket1 = Proxy_Server('localhost', 8001)
-print("Got connection: ", socket1.address)
+    def handle_connection(self, conn, address):
+
+        print("Got connection from", address)
+
+        while True:
+
+            process_id = os.getpid()
+            print("Process id:", process_id)
+
+            # Recieve message from the client
+            message = conn.recv(2024)
+
+            hostname = message.decode('utf-8').rstrip('\n').rstrip('\r')
+
+            try:
+                host_ip = socket.gethostbyname(hostname)
+            except socket.gaierror:
+                print("Error with resolving host name")
+            # print(host_ip)
+
+            # Requests a page
+            request = "GET / HTTP/1.1\r\nHost: " + host_ip + "\n\n"
+
+            conn.sendall(request.encode())
+        conn.close()
+
+if __name__ == '__main__':
+        ser = Proxy_Server('localhost', 8005)
+        print("Starting server......")
+        ser.server()
